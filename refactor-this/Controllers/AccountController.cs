f7ff652d7227d0c069be70640f07d35b5a9b6281
Milad Katebi <mldktb@gmail.com>
2020-10-22 23:04:02 +1300

    Add project files.

---
+++ refactor-this/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using refactor_this.DatabaseHelpers;
+using refactor_this.Models;
+using System;
+using System.Configuration;
+using System.Web.Http;
+
+namespace refactor_this.Controllers
+{
+    public class AccountController : ApiController
+    {
+        private readonly AccountRepository Repository;
+
+        public AccountController()
+        {
+            Repository = new AccountRepository(ConfigurationManager.
+                ConnectionStrings["conStr"].ConnectionString);
+        }
+
+        [HttpGet, Route("api/Accounts/{id}")]
+        public IHttpActionResult GetById(Guid id)
+        {
+            return Ok(Repository.GetById(id));
+        }
+
+        [HttpGet, Route("api/Accounts")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Repository.GetAll());
+        }
+
+        [HttpPost, Route("api/Accounts")]
+        public IHttpActionResult Add(Account account)
+        {
+            Repository.Add(account);
+            return Ok();
+        }
+
+        [HttpPut, Route("api/Accounts/{id}")]
+        public IHttpActionResult Update(Account account)
+        {
+            Repository.Update(account);
+            return Ok();
+        }
+
+        [HttpDelete, Route("api/Accounts/{id}")]
+        public IHttpActionResult Delete(Guid id)
+        {
+            var account = Repository.GetById(id);
+            Repository.Delete(account);
+            return Ok();
+        }
+    }
+}
---
+++ refactor-this/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace refactor_this.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public float Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ForeignKey("AccountId")]
+        public Guid AccountId { get; set; }
+    }
+}
